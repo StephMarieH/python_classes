55caf0a9c825a86f8ed3f15e8cbbe5d4bf0ebf9f
Stephanie Hornby <shornby@ABLAB.local>
2024-06-10 14:20:22 +0100

    methods added

--- classes_exercise.py
+++ classes_exercise.py
@@ -1,47 +1,55 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x=float, y=float):
         self.x = x
         self.y = y
 
 
-class Shape(Point, ABC):
+class Shape(ABC, Point):
     
-    def __init__(self, name=str, colour=str, x, y):
+    def __init__(self, name=str, colour=str, x=float, y=float):
         super().__init__(x, y)
         self.name = name
         self.colour = colour
     
     @abstractmethod
-    def getArea(self):
+    def getArea(self) -> float:
         # return float
         pass
     
     @abstractmethod
-    def getCenterPoint(self):
+    def getCenterPoint(self) -> float:
         # return float
         pass
 
 
 class Rectangle(Shape):
-    def __init__(self, name=str, colour=str, x, y, height, width):
+    def __init__(self, name=str, colour=str, x=float, y=float, height=float, width=float):
         super().__init__(self, name, colour, x, y)
         self.height = height
         self.width = width
 
-    def isSquare(height, width):
-        # return bool
-        if height == width:
-            return True
-        else:
-            return False
+    def getArea(self) -> float:
+        return self.height * self.width
+
+    def getCenterPoint(self) -> Point:
+        centerX = self.x + self.width / 2
+        centerY = self.y + self.height / 2
+        return Point(centerX, centerY)
+
+    def isSquare(self) -> bool:
+        return self.height == self.width
 
 
 class Circle(Shape):
-    def __init__(self, name=str, colour=str, x, y, height, width):
+    def __init__(self, name=str, colour=str, x=float, y=float, radius=float):
         super().__init__(self, name, colour, x, y)
-        self.height = height
-        self.width = width
+        self.radius = radius
 
-    
+    def getArea(self) -> float:
+        import math
+        return math.pi * (self.radius ** 2)
+
+    def getCenterPoint(self) -> Point:
+        return Point(self.x, self.y)
